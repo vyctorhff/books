7124afd6af6954749e2fdafb67a3abcc4548334e
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-07-03 09:01:36 -0300

    data-structure: cap 4

---
+++ data-structure/src/main/java/base/cap4/Complexity.java
@@ -0,0 +1,15 @@
+package base.cap4;
+
+/**
+ * Função constante
+ *      - XXXXXXXX
+ * Função linear
+ * Função logarítmica
+ * Função n-log n
+ * Função quadrática
+ * Função cúbica
+ * Função exponencial
+ *
+ */
+public class Complexity {
+}
