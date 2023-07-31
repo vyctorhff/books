65d933a986b54927b11a94ba611c3232da08c10b
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-07-31 11:03:13 -0300

    data-structure: mixing and question

---
+++ data-structure/src/main/java/br/study/mixing/base64/Base64Impl.java
@@ -0,0 +1,10 @@
+package br.study.mixing.base64;
+
+/**
+ * Implement base64 transformation
+ *
+ * Rules:
+ *
+ */
+public class Base64Impl {
+}
---
+++ data-structure/src/main/java/br/study/mixing/happyNumber/HappyNumberQuestion.java
@@ -0,0 +1,30 @@
+package br.study.mixing.happyNumber;
+
+/**
+ * Para um número natural >= 1.
+ * A number will be happy if the sum with each digit exp 2 is equal to 1.
+ * Atenção para o número que fica no loop infinito.
+ *
+ * Ex: 32
+ * 3^2 + 2^2 = 13
+ * 1^2 + 3^2 = 10
+ * 1^2 + 0^2 = 1
+ * happy!
+ *
+ * Ex: 37
+ * 32 + 72 = 58
+ * 5^2 + 8^2 = 89
+ * 8^2 + 9^2 = 145
+ * 1^2 + 4^2 + 5^2 = 42
+ * 4^2 + 2^2 = 20
+ * 2^2 + 0^2 = 4
+ * 4^2 = 16
+ * 1^2 + 6^2 = 37
+ * Sad!
+ *
+ * Ex: 4:
+ * 4^2 = 16
+ * 1^2 + 6^2 = 37
+ */
+public class HappyNumberQuestion {
+}
