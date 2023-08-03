57a2fdb82556d0528ea7cd02072fe13d812a4ed8
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-03 16:25:14 -0300

    data-structure: base64

--- data-structure/src/main/java/br/study/mixing/base64/Base64Impl.java
+++ data-structure/src/main/java/br/study/mixing/base64/Base64Impl.java
@@ -3,8 +3,22 @@ package br.study.mixing.base64;
 /**
  * Implement base64 transformation
  *
- * Rules:
- *
+ * How to encode:
+ * 1 - Take a character sequence
+ * 2 - Convert each chracter to ASCII(unicode) decimal
+ * 3 - Convert each decimal to binary
+ * 4 - Convert the 8 bits in 6 bits representation
+ * 5 - Convert each 6 bits sequence to a number
+ * 6 - Convert each number to a character based on base 64 table
+ * 7 - Concat the role sequence to a string
  */
 public class Base64Impl {
+
+//    public String encode(String input) {
+//        return null;
+//    }
+
+//    public String decode(String input) {
+//        return null;
+//    }
 }
