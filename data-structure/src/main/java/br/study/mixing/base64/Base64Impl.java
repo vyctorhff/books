9acf454c2672c4d87776495ed08f93bd45ed193f
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-07 09:24:40 -0300

    data-structure: doc base64

--- data-structure/src/main/java/br/study/mixing/base64/Base64Impl.java
+++ data-structure/src/main/java/br/study/mixing/base64/Base64Impl.java
@@ -8,17 +8,26 @@ package br.study.mixing.base64;
  * 2 - Convert each chracter to ASCII(unicode) decimal
  * 3 - Convert each decimal to binary
  * 4 - Convert the 8 bits in 6 bits representation
+ *  4.1 - Padding
+ *  4.2 - if the when group bits in 6 bits and someone not complete 6 bits
+ *  4.3 - it should add to the right with zeros
+ *  4.4 - if it needs 2 zeros, add a '=' at the final of the result
+ *  4.5 - if it needs 4 zeros, add a '==' at the final of the result
  * 5 - Convert each 6 bits sequence to a number
  * 6 - Convert each number to a character based on base 64 table
  * 7 - Concat the role sequence to a string
+ *
+ * How to decode:
+ * 1 - Take a base64 character sequence
+ * 2 -
  */
 public class Base64Impl {
 
-//    public String encode(String input) {
-//        return null;
-//    }
+    public String encode(String input) {
+        return null;
+    }
 
-//    public String decode(String input) {
-//        return null;
-//    }
+    public String decode(String input) {
+        return null;
+    }
 }
