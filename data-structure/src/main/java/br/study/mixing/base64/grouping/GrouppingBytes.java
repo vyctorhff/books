34066cc7a7b30b4119809f72379239b9837e7ab7
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-08 08:53:41 -0300

    data-structure: base64 grouping

---
+++ data-structure/src/main/java/br/study/mixing/base64/grouping/GrouppingBytes.java
@@ -0,0 +1,6 @@
+package br.study.mixing.base64.grouping;
+
+public interface GrouppingBytes {
+
+    String process(String string);
+}
--- data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupSixPartsInBytes.java
+++ data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupSixPartsInBytes.java
@@ -1,10 +1,8 @@
 package br.study.mixing.base64.grouping.impl;
 
 import br.study.mixing.base64.grouping.GrouppingBytes;
-import lombok.Data;
 
-@Data
-public class GroupByteInSixParts implements GrouppingBytes {
+public class GroupSixPartsInBytes implements GrouppingBytes {
     @Override
     public String process(String string) {
         return null;
