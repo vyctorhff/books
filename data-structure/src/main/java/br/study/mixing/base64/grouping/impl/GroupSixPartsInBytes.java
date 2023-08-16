c2ddb7601e2288eb58465a0a64e464c1d7b0851c
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-16 14:42:24 -0300

    data-structure: base64 groups

--- data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupSixPartsInBytes.java
+++ data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupSixPartsInBytes.java
@@ -2,6 +2,12 @@ package br.study.mixing.base64.grouping.impl;
 
 import br.study.mixing.base64.grouping.GrouppingBytes;
 
+/**
+ * Group 8 parts in 6 parts.
+ * Ex:
+ * input: 000000 000000 000000 000000
+ * output: 00000000 00000000 00000000
+ */
 public class GroupSixPartsInBytes implements GrouppingBytes {
     @Override
     public String process(String string) {
--- data-structure/src/test/java/br/study/mixing/base64/grouping/impl/GroupByteInSixPartsTest.java
+++ data-structure/src/test/java/br/study/mixing/base64/grouping/impl/GroupByteInSixPartsTest.java
@@ -1,23 +1,33 @@
 package br.study.mixing.base64.grouping.impl;
 
-import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
+import org.junit.platform.commons.util.StringUtils;
 
-import java.util.Arrays;
+import java.util.stream.Stream;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 
 class GroupByteInSixPartsTest {
 
     private GroupByteInSixParts sut;
 
-    @BeforeEach
-    void setup() {
+    @ParameterizedTest
+    @MethodSource("sourceGroupParts")
+    void shouldGroupParts(String input, String expected) {
+        String result = sut.process(input);
+
+        assertFalse(StringUtils.isNotBlank(result));
+        assertEquals(expected, result);
     }
 
-    @Test
-    void shouldGroupThreeBytesInFourParts() {
-        var input = "00000000 00000000 00000000";
-        var expected = "000000 000000 000000 000000";
+    private static Stream<Arguments> sourceGroupParts() {
+        return Stream.of(
+            Arguments.of("00000000", "000000 00"),
+            Arguments.of("00000000 00000000", "000000 000000 0000"),
+            Arguments.of("00000000 00000000 00000000", "000000 000000 000000 000000")
+        );
     }
 }
