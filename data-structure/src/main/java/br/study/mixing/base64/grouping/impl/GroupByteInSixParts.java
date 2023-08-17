7897f36a62178409e3db8f19d1409ba13c2c19c8
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-17 09:51:48 -0300

    data-structure: base64 group parts

--- data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupByteInSixParts.java
+++ data-structure/src/main/java/br/study/mixing/base64/grouping/impl/GroupByteInSixParts.java
@@ -2,6 +2,10 @@ package br.study.mixing.base64.grouping.impl;
 
 import br.study.mixing.base64.grouping.GrouppingBytes;
 import lombok.Data;
+import org.apache.commons.lang3.StringUtils;
+
+import java.util.ArrayList;
+import java.util.Arrays;
 
 /**
  * Group 8 parts in 6 parts.
@@ -12,7 +16,24 @@ import lombok.Data;
 @Data
 public class GroupByteInSixParts implements GrouppingBytes {
     @Override
-    public String process(String string) {
-        return null;
+    public String process(String content) {
+        if (StringUtils.isBlank(content)) {
+            throw new IllegalArgumentException("Could not group, invalid content");
+        }
+
+        var listBytes = Arrays.stream(content.split(StringUtils.SPACE));
+        var listResult = new ArrayList<String>();
+
+        listBytes.forEach(groupByte -> {
+            transform(groupByte, listResult);
+        });
+
+        return StringUtils.join(listBytes, StringUtils.SPACE);
+    }
+
+    private void transform(String groupByte, ArrayList<String> listResult) {
+        // check how much left for the last item in the array
+        // if none then add 6 numbers and 2 in the next index of the list
+        // else add 2 numbers and 6 in the next index of the list
     }
 }
