9fc4499e3689a63561d60ebb66e1a61a0729c50f
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-28 16:46:11 -0300

    data-structure: base 64 convert number to binary

--- data-structure/src/main/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinary.java
+++ data-structure/src/main/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinary.java
@@ -1,7 +1,18 @@
 package br.study.mixing.base64.converts.impl;
 
+import org.apache.commons.lang3.StringUtils;
+
 /**
- * Convert a decimal to its binary representation
+ * Convert an integer to its binary representation
  */
 public class ConvertNumberAndBinary {
+
+    public String convertToBinary(int value) {
+        String strBinary = Integer.toBinaryString(value);
+        return StringUtils.leftPad("0", 7);
+    }
+
+    public int convertToNumber(String value) {
+        return Integer.parseInt(value, 2);
+    }
 }
---
+++ data-structure/src/test/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinaryTest.java
@@ -0,0 +1,11 @@
+package br.study.mixing.base64.converts.impl;
+
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+@ExtendWith({MockitoExtension.class})
+class ConvertNumberAndBinaryTest {
+
+}
