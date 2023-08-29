7c9613a6a922420be108f8f7f7fc2473b444c8cb
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-08-29 08:47:59 -0300

    data-structure: base64 - number to binary

--- data-structure/src/main/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinary.java
+++ data-structure/src/main/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinary.java
@@ -7,12 +7,20 @@ import org.apache.commons.lang3.StringUtils;
  */
 public class ConvertNumberAndBinary {
 
+    public static final int BYTE_SIZE = 8;
+    public static final int BASE_NUMBER_BINARY = 2;
+    public static final String DEFAULT_BIT = "0";
+
     public String convertToBinary(int value) {
         String strBinary = Integer.toBinaryString(value);
-        return StringUtils.leftPad("0", 7);
+        return StringUtils.leftPad(strBinary, BYTE_SIZE, DEFAULT_BIT);
     }
 
     public int convertToNumber(String value) {
-        return Integer.parseInt(value, 2);
+        if (StringUtils.isBlank(value) || (value.length() != BYTE_SIZE)) {
+            throw new IllegalArgumentException("");
+        }
+
+        return Integer.parseInt(value, BASE_NUMBER_BINARY);
     }
 }
--- data-structure/src/test/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinaryTest.java
+++ data-structure/src/test/java/br/study/mixing/base64/converts/impl/ConvertNumberAndBinaryTest.java
@@ -1,11 +1,39 @@
 package br.study.mixing.base64.converts.impl;
 
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.Arguments;
+import org.junit.jupiter.params.provider.MethodSource;
 import org.mockito.junit.jupiter.MockitoExtension;
 
-import static org.junit.jupiter.api.Assertions.*;
+import java.util.stream.Stream;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 @ExtendWith({MockitoExtension.class})
 class ConvertNumberAndBinaryTest {
 
+    private ConvertNumberAndBinary sut;
+
+    @BeforeEach
+    void setup() {
+        this.sut = new ConvertNumberAndBinary();
+    }
+
+    @ParameterizedTest
+    @MethodSource("sourceConvertForResult")
+    void shouldConvertForResult(int value, String expected) {
+        var result = this.sut.convertToBinary(value);
+        assertEquals(expected, result);
+    }
+
+    static Stream<Arguments> sourceConvertForResult() {
+        return Stream.of(
+            Arguments.of(0, "00000000"),
+            Arguments.of(1, "00000001"),
+            Arguments.of(3, "00000011"),
+            Arguments.of(255, "11111111")
+        );
+    }
 }
