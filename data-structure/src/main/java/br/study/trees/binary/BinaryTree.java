fc0e68c93d9f7472e39ac3745c2b715fb6684d31
Victor Hugo Ferreira de Figueiredo <vyctorhff@gmail.com>
2023-10-11 16:50:23 -0300

    data-structure: trees - move doc

--- data-structure/src/main/java/br/study/trees/binary/BinaryTree.java
+++ data-structure/src/main/java/br/study/trees/binary/BinaryTree.java
@@ -2,10 +2,6 @@ package br.study.trees.binary;
 
 import org.apache.commons.lang3.NotImplementedException;
 
-/**
- * Própria(ou cheias)
- * É uma árvore binharia que tem exatamente zero ou dois filhos em cada nó
- */
 public class BinaryTree<E> {
 
     private BTNode<E> root;
